22987fba91573a5c64f29a5c20ed6cdda8d36b74
Tiger HU <tigerlilyh777@gmail.com>
2025-06-17 00:07:47 +1200

    Add asFlow() and flowOf()

--- app/src/main/java/com/example/androidflow/FlowBasics.kt
+++ app/src/main/java/com/example/androidflow/FlowBasics.kt
@@ -2,20 +2,26 @@ package com.example.androidflow
 
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.runBlocking
 
-fun simple(): Flow<Int> = flow {
+fun simpleFlow(): Flow<Int> = flow {
     for (i in 1..5) {
         delay(1000)
         emit(i)
     }
 }
 
+fun rangeAsFlow(): Flow<Int> = (1..3).asFlow()
+
+fun stringFlow(): Flow<String> = flowOf("one", "two", "three")
+
 fun main() {
     runBlocking {
-        simple().collect {
-            println(it)
-        }
+        simpleFlow().collect { println(it) }
+        rangeAsFlow().collect { println(it) }
+        stringFlow().collect { println(it) }
     }
 }
